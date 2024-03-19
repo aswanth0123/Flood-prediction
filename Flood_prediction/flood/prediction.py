535e98ca0697f8f04471c21a918b02aa75f89a0f
aswanth0123 <aswanthsuresh74@gmail.com>
2024-03-19 10:49:47 +0530

    final modifications

--- Flood_prediction/flood/prediction.py
+++ Flood_prediction/flood/prediction.py
@@ -5,9 +5,7 @@ from sklearn.preprocessing import LabelEncoder,MinMaxScaler
 data=pd.read_csv("flood/kerala.csv")
 data.drop("SUBDIVISION",axis=1,inplace=True)
 x_input=data.drop("FLOODS",axis=1)
-# x_input = x_input.drop("YEAR",axis = 1)
 y_output=data["FLOODS"]
-# year = data["YEAR"]
 
 ldr=LabelEncoder()
 y_output=ldr.fit_transform(y_output)
@@ -32,7 +30,7 @@ from sklearn.metrics import accuracy_score
 
 score = accuracy_score(prediction,y_test)
 
-print("accuracy score is", score)
+# print("accuracy score is", score)
 
 
 def predict1(year,monthly):
@@ -40,12 +38,9 @@ def predict1(year,monthly):
     monthly_data = monthly
     annual_fall = round(sum(monthly_data),3)
     monthly_data.append(annual_fall)
-    print(monthly_data)
     input_data = []
     input_data.append(year)
     input_data.extend(monthly_data)
     input_array = np.array(input_data).reshape(1,len(input_data))
-    print(input_array)
     prediction = model.predict(input_array)
-    print(prediction)
     return prediction
